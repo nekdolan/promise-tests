3139cf5c48db8ccc785acb1f95b83a9b0cb4e129
nekdolan <nekdolan@protonmail.com>
2018-03-31 14:45:35 -0700

    ver 4

--- promise.js
+++ promise.js
@@ -1,13 +1,26 @@
+new Promise((resolve) => resolve(1))
+  .then(() => {throw(2)})
+  .then(null, () => Promise.resolve(3))
+  .catch((e) => console.log('Does not get called after exception'))
+  .then(() => console.log('Gets called after catch'));
+
 Promise.prototype.then = (function () {
   const then = Promise.prototype.then;
-  const fixCall = function(promise, next, error){
+  const fixCall = function(promise, next){
+    if (!next) {
+      return null;
+    }
     return function (val) {
       try {
-        return next.call(promise, val);
+        let newPromise = next.call(promise, val);
+        if(newPromise){
+          newPromise.error = promise.error;
+        }
+        return newPromise;
       } catch (exception) {
         setTimeout(function () {
-          if (error) {
-            error(exception);
+          if (promise.error) {
+            promise.error(exception);
           } else {
             throw(exception);
           }
@@ -17,16 +30,33 @@ Promise.prototype.then = (function () {
     }
   };
   return function (success, fail, error) {
-    return then.call(this, fixCall(this, success, error), fixCall(this, fail, error));
+    let promise = then.call(this, fixCall(this, success), fixCall(this, fail));
+    promise.error = this.error;
+    return promise;
   }
 }());
 
-new Promise((resolve) => resolve('ok'))
+function createPromise(init, error){
+  let promise = new Promise(init);
+  promise.error = error;
+  return promise;
+}
+
+createPromise((resolve) => resolve(), (e) => console.warn('Error caught'))
+  .then(() => 5)
+  .then(() => {throw new Error('error')});
+
+createPromise((resolve) => resolve('value'))
   .then((val) => Promise.reject(val))
   .then(null, (val) => {
-    console.log('value rejected: ' + val);
-    throw new Error('lol');
-  }, (error) => {
-    console.log(error)
+    console.log('Value rejected: ' + val);
+    throw new Error('Will throw exception');
   })
-  .then(() => console.log('not called'));
+  .then(() => console.log('Not called'));
+
+
+// new Promise((resolve) => resolve(1))
+//   .then(() => {throw(2)})
+//   .then(null, () => Promise.resolve(3))
+//   .catch((e) => console.log('does not get called'))
+//   .then(() => console.log('gets called'));
